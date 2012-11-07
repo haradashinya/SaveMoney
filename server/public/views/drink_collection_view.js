8f84767b99c43413c17f600df4c3800ce6bd57c9
haradashinya <haradashinya@gmail.com>
2012-11-07 17:43:55 +0900

    use require.js/text plugin

--- server/public/views/drink_collection_view.js
+++ server/public/views/drink_collection_view.js
@@ -1,14 +1,20 @@
-define(["zepto","underscore","backbone"],function($,_,Backbone){
+define(["zepto","underscore","backbone","lib/text!templates/edit_drink.html"],function($,_,Backbone,template){
+
+
 	var DrinkCollectionView = Backbone.View.extend({
 		el: "#content",
 		initialize:function(){
 			console.log(this.$el.html);
+			console.log(template);
 			_.bindAll(this,"render");
 
 		},
 		render:function(){
-			this.$el.html("fffffffff");
-
+			var data = {
+				name: "hello world"
+			};
+			var compiledTemplate = _.template(template,data);
+			this.$el.html(compiledTemplate);
 		}
 	});
 
