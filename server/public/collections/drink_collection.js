6bc9275f5722254d242fca17ec68fb2eb3d2c422
haradashinya <haradashinya@gmail.com>
2012-11-07 20:58:02 +0900

    work in progress

--- server/public/collections/drink_collection.js
+++ server/public/collections/drink_collection.js
@@ -5,18 +5,11 @@ define(["zepto","underscore","backbone","drink"],
 				return "http://localhost:9393/users/11/drinks/";
 			},
 			initialize:function(uuid){
-				console.log("fffff");
-				var self = this;
-				this.fetch({
-					success:function(data){
-						console.log(data.models);
-					}
-				})
 			},
 			setUUID:function(uuid){
 				this.uuid = uuid;
 				console.log(this.uuid);
-			},
+			}
 
 		});
 
--- server/public/views/drink_collection_view.js
+++ server/public/views/drink_collection_view.js
@@ -1,12 +1,24 @@
-define(["zepto","underscore","backbone","lib/text!templates/edit_drink.html","drink_collection"],
-	function($,_,Backbone,template,DrinkCollection){
+define(["zepto","underscore","backbone","lib/text!templates/edit_drink.html","drink_collection","drink"],
+	function($,_,Backbone,template,DrinkCollection,Drink){
 
 
 		var DrinkCollectionView = Backbone.View.extend({
 			el: "#content",
 			collection: "",
 			initialize:function(){
-				_.bindAll(this,"render");
+				_.bindAll(this,"render","update");
+				this.collection.fetch({
+					success:$.proxy(this.addView,this)
+				})
+
+			},
+			addView:function(collection,resp){
+				var self = this;
+				collection.models.forEach(function(item){
+					// make new DrinkView like this
+//					var drinkView = new DrinkView;
+					// self.$el.append(drinkView.render());
+				})
 			},
 			render:function(uuid){
 				var data = {
@@ -14,6 +26,9 @@ define(["zepto","underscore","backbone","lib/text!templates/edit_drink.html","dr
 				};
 				var compiledTemplate = _.template(template,data);
 				this.$el.html(compiledTemplate);
+			},
+			update:function(){
+				console.log("update");
 			}
 		});
 
