d1660151669895b610d84d0a939fe094155cb10b
haradashinya <haradashinya@gmail.com>
2012-11-08 11:42:01 +0900

    add template #drink_view

--- server/public/views/drink_view.js
+++ server/public/views/drink_view.js
@@ -1,15 +1,17 @@
-define(["zepto","underscore","backbone"],function($,_,Backbone){
-	var DrinkView = Backbone.View.extend({
-		initialize:function(){
+define(["zepto","underscore","backbone","lib/text!templates/edit_drink.html"],
+	function($,_,Backbone,template){
+		var DrinkView = Backbone.View.extend({
+			initialize:function(){
+			},
+			render:function(){
+				console.log(this.model);
+				console.log(template);
+				this.$el.html("<li>hello world</li>")
+				return this;
+			}
 
-		},
-		render:function(){
-			this.$el.html("<li>hello world</li>")
-			return this;
-		}
+		});
 
+
+		return DrinkView;
 	});
-
-
-	return DrinkView;
-});
