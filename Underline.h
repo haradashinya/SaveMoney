2216b612a227d6e1abfcd5f0035a66adf62abf30
haradashinya <haradashinya@gmail.com>
2012-11-13 16:17:22 +0900

    add underline

---
+++ Underline.h
@@ -0,0 +1,13 @@
+//
+//  Underline.h
+//  SaveMoney
+//
+//  Created by HARADA SHINYA on 11/13/12.
+//  Copyright (c) 2012 HARADA SHINYA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Underline : UILabel
+
+@end
