d665a989b94e9f30a3a8cf77e86c7cfa18934e13
haradashinya <haradashinya@gmail.com>
2012-11-13 10:06:58 +0900

    pass uuid to client side

--- server/public/collections/drink_collection.js
+++ server/public/collections/drink_collection.js
@@ -2,8 +2,7 @@ define(["zepto","underscore","backbone","drink"],
 	function($,_,Backbone,Drink){
 		var DrinkCollection = Backbone.Collection.extend({
 			url:function(){
-				console.log(this.uuid);
-				return "http://localhost:9393/users/" +"0D20B5EE-87AD-4C60-BD39-B8DFA2A21CC5" +  "/drinks/";
+				return "http://localhost:9393/users/" + this.uuid +  "/drinks/";
 			},
 			initialize:function(uuid){
 			},
