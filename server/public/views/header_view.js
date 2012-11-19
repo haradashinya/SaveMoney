bbc70bf4cfd02e774897c631a854125df8375d93
haradashinya <haradashinya@gmail.com>
2012-11-19 18:39:30 +0900

    add HeaderView

---
+++ server/public/views/header_view.js
@@ -0,0 +1,13 @@
+define(["zepto","underscore","backbone"],function($,_,Backbone){
+	var HeaderView = Backbone.View.extend({
+			initialize:function(){
+			},
+			render:function(){
+				// this.$el.html("hello world");
+				return this;
+			}
+	});
+
+
+	return HeaderView;
+});
