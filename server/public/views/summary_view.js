80aa7557190156013a438d329c8256a81aabdcad
haradashinya <haradashinya@gmail.com>
2012-11-20 17:28:10 +0900

    add summary template

--- server/public/views/summary_view.js
+++ server/public/views/summary_view.js
@@ -1,10 +1,15 @@
-define(["zepto","underscore","backbone"],function($,_,Backbone){
+define(["zepto","underscore","backbone","lib/text!templates/summary.html"],function($,_,Backbone,template){
 	var SummaryView = Backbone.View.extend({
 			
 			filteredDrinks:'',
 			initialize:function(){
 				this.collection.calclateCountByType();
-
+				this.render();
+			},
+			render:function(){
+				var tmp = _.template(template,{});
+				this.$el.html(tmp);
+				return this;
 			}
 	});
 
